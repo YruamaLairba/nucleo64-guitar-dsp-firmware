507e735eacfd16df851ae4bd4526bc86c891a5df
Yruama_Lairba <yruama_lairba@hotmail.com>
2021-07-25 00:40:05 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 //! examples/init.rs
 
 #![deny(unsafe_code)]
-#![deny(warnings)]
+//#![deny(warnings)]
 #![no_main]
 #![no_std]
 
 use core::panic::PanicInfo;
 use cortex_m_semihosting::debug;
 use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal::{stm32};
+use stm32;
 //use stm32f4::stm32f411;
+
+//PLLI2S clock configuration
+const PLLI2SM: u8 = 5;
+const PLLI2SN: u16 = 192;
+const PLLI2SR: u8 = 5;
+
+//Clock configuration of the used i2s interface
+const I2SDIV: u8 = 2;
+const ODD: bool = true;
+
+//generate Master Clock ? Modifying this require to adapt the i2s clock
+const MCK: bool = true;
 
 #[rtic::app(device = stm32f4::stm32f411, peripherals = true)]
 const APP: () = {
@@ -16,6 +30,22 @@ const APP: () = {
     fn init(_: init::Context) {
         rtt_init_print!();
         rprintln!("init");
+    let device = stm32::Peripherals::take().unwrap();
+    let gpioa = device.GPIOA.split();
+    let gpiob = device.GPIOB.split();
+    let gpioc = device.GPIOC.split();
+    let rcc = device.RCC.constrain();
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.mhz())
+        .sysclk(96.mhz())
+        .hclk(96.mhz())
+        .pclk1(50.mhz())
+        .pclk2(100.mhz())
+        .freeze();
+
+
+
     }
 
     #[idle]
