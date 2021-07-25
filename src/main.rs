455349fd4a1b795a929b7811fa9cfcdc2c1df784
Yruama_Lairba <yruama_lairba@hotmail.com>
2021-07-25 02:11:06 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! examples/init.rs
 
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 //#![deny(warnings)]
 #![no_main]
 #![no_std]
@@ -8,8 +8,8 @@
 use core::panic::PanicInfo;
 use cortex_m_semihosting::debug;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32f4xx_hal::{stm32};
-use stm32;
+use stm32f4xx_hal::{prelude::*,stm32,spi};
+use spi::{Spi};
 //use stm32f4::stm32f411;
 
 //PLLI2S clock configuration
@@ -30,22 +30,228 @@ const APP: () = {
     fn init(_: init::Context) {
         rtt_init_print!();
         rprintln!("init");
-    let device = stm32::Peripherals::take().unwrap();
-    let gpioa = device.GPIOA.split();
-    let gpiob = device.GPIOB.split();
-    let gpioc = device.GPIOC.split();
-    let rcc = device.RCC.constrain();
-    let clocks = rcc
-        .cfgr
-        .use_hse(8.mhz())
-        .sysclk(96.mhz())
-        .hclk(96.mhz())
-        .pclk1(50.mhz())
-        .pclk2(100.mhz())
-        .freeze();
+        let device = stm32::Peripherals::take().unwrap();
+        let gpioa = device.GPIOA.split();
+        let gpiob = device.GPIOB.split();
+        let gpioc = device.GPIOC.split();
+        let rcc = device.RCC.constrain();
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.mhz())
+            .sysclk(96.mhz())
+            .hclk(96.mhz())
+            .pclk1(50.mhz())
+            .pclk2(100.mhz())
+            .freeze();
+        //enable system clock for APB1 bus and SPI2 (I2S2)
+        unsafe {
+            let rcc = &(*stm32::RCC::ptr());
+            rcc.apb1enr
+                .modify(|_, w| w.pwren().set_bit().spi2en().set_bit());
+        }
+        //setup  and startup PLLI2S clock
+        unsafe {
+            let rcc = &(*stm32::RCC::ptr());
+            //setup
+            rcc.plli2scfgr.modify(|_, w| {
+                w.plli2sr().bits(PLLI2SR);
+                w.plli2sn().bits(PLLI2SN);
+                w.plli2sm().bits(PLLI2SM)
+            });
+            //run the clock
+            rcc.cr.modify(|_, w| w.plli2son().set_bit());
+            //wait a stable clock
+            while rcc.cr.read().plli2srdy().bit_is_clear() {}
+        }
+        //Setup Spi1
+        let pa5 = gpioa.pa5.into_alternate_af5(); //CK
+        let pa7 = gpioa.pa7.into_alternate_af5(); //MOSI
+        let mut pb2 = gpiob.pb2.into_push_pull_output(); //CS
+        let _ = pb2.set_high();
 
+        let spi1_mode = spi::Mode {
+            polarity: spi::Polarity::IdleHigh,
+            phase: spi::Phase::CaptureOnSecondTransition, //With IdleHigh, capture on rising edge
+        };
 
-
+        let mut spi1 = Spi::spi1(
+            device.SPI1,
+            (pa5, spi::NoMiso, pa7),
+            spi1_mode,
+            1.mhz().into(),
+            clocks,
+        );
+        //Setup wm8731 chip
+        //
+        //Left Line in
+        // 4:0 LINVOL, 10111 = 0db
+        // 7 LINMUTE, 1 = mute
+        // 8 LRINBOTH, 1 = apply VOL and MUTE to L & R channel
+    #[allow(clippy::unusual_byte_groupings)]
+        let spi_buf = [0b000_0000_1,0b00010111];
+        let _ = pb2.set_low();
+        spi1.write(&spi_buf).unwrap();
+        let _ = pb2.set_high();
+        //Left Headphone Out
+        // 6:0 HPVOL,1111001= 0db,0110000 = -73dB, 0000000 to 0101111 = MUTE
+        // 7 LZCEN, enable zero cross detection
+        // 8 LRHPBOTH, 
+        #[allow(clippy::unusual_byte_groupings)]
+        let spi_buf = [0b000_0010_1,0b01111001-12];
+        let _ = pb2.set_low();
+        spi1.write(&spi_buf).unwrap();
+        let _ = pb2.set_high();
+        //Analogue Audio Path Control:
+        // 0 mic boost disabled: 0
+        // 1 mic muted: 1
+        // 2 INSEL : 0 = Line input to ADC, 1 = microphone to ADC
+        // 3 BYPASS: connect Line IN to Line Out
+        //
+        // 4 DACSEL: connect DAC to Line Out
+        // 5 SIDETONE: connect mic to line out
+        // 7:6 SIDEATT: side tone attenuation (see p51)
+    #[allow(clippy::unusual_byte_groupings)]
+        let spi_buf = [0b000_0100_0,0b0001_0010];
+        let _ = pb2.set_low();
+        spi1.write(&spi_buf).unwrap();
+        let _ = pb2.set_high();
+        //Digital Audi Path Control
+        // 0 ADCHPD: ADC high pass enable
+        // 2:1 DEEMP: de-emphasis control (see p52)
+        // 3 DACMU: dac soft mute
+        //
+        // 4 HPOR: Store dc offset when High Pass Filter disabled (p52)
+    #[allow(clippy::unusual_byte_groupings)]
+        let spi_buf = [0b000_0101_0,0b0000_0000];
+        let _ = pb2.set_low();
+        spi1.write(&spi_buf).unwrap();
+        let _ = pb2.set_high();
+        //Power down control : 1 = power down, 0 = power up
+        // 0 LINEPD
+        // 1 MICPD
+        // 2 ADCPD
+        // 3 DACPD
+        //
+        // 4 OUTPD
+        // 5 OSCPD
+        // 6 CLKOUTPD
+        // 7 POWEROFF : enable power off mode
+    #[allow(clippy::unusual_byte_groupings)]
+        let spi_buf = [0b000_0110_0,0b0110_0010];
+        let _ = pb2.set_low();
+        spi1.write(&spi_buf).unwrap();
+        let _ = pb2.set_high();
+        //Digital Audio Interface Format
+        // 1:0 FORMAT (see p53) , I2S = 10
+        // 3:2 IWL: Input audio data bit length, 32bits = 11
+        // 
+        // 4 LRP: DACLRC phase control, to check later, default=0
+        // 5 LRSWAP: DAC Left Right Clock Swap ? default=0
+        // 6 MS: Master/Slave control: Slave=0
+        // 7 BCLKINV: invert bit clock: Don't invert=0(default)
+    #[allow(clippy::unusual_byte_groupings)]
+        let spi_buf = [0b000_0111_0,0b0000_1110];
+        let _ = pb2.set_low();
+        spi1.write(&spi_buf).unwrap();
+        let _ = pb2.set_high();
+        //Sampling Control
+        // 0 USB/NORMAL : 0 = Normal
+        // 1 BOSR: base oversampling 0=256fs for Normal
+        // 5:2 SR: ADc and DAC sampling rate (p42-45)
+        // 6 CLKDIV2: divide core clock by 2 (0= core clock = MCLK)
+    #[allow(clippy::unusual_byte_groupings)]
+        let spi_buf = [0b000_1000_0,0b0000_0000];
+        let _ = pb2.set_low();
+        spi1.write(&spi_buf).unwrap();
+        let _ = pb2.set_high();
+        //Active Control
+        //0 ACTIVE : 1 activate. The interface should be disabled when changing conf
+    #[allow(clippy::unusual_byte_groupings)]
+        let spi_buf = [0b000_1001_0,0b0000_0001];
+        let _ = pb2.set_low();
+        spi1.write(&spi_buf).unwrap();
+        let _ = pb2.set_high();
+        //Setup i2s2 and i2s2_ext
+        //gpio
+        let _pb13 = gpiob.pb13.into_alternate_af5(); //CK
+        let _pb15 = gpiob.pb15.into_alternate_af5(); //SD
+        let _pb14 = gpiob.pb14.into_alternate_af6(); //ext_SD
+        let _pb12 = gpiob.pb12.into_alternate_af5(); //WS
+        let _pc6 = gpioc.pc6.into_alternate_af5(); //MCK
+        //Setup an interrupt that can be triggered by pb12 pin
+        //Note: The hal doesn't allow to manipulate interrupt for pin in aternate mode
+        unsafe {
+            let syscfg = &(*stm32::SYSCFG::ptr());
+            //interrupt on pb12
+            syscfg.exticr4.modify(|_, w| w.exti12().bits(0b0001));
+            let exti = &(*stm32::EXTI::ptr());
+            //mask EXTI0 interrupt
+            exti.imr.modify(|_, w| w.mr12().set_bit());
+            //trigger interrupt on rising edge
+            exti.rtsr.modify(|_, w| w.tr12().set_bit());
+            //unmask EXTI0 interrupt
+            stm32::NVIC::unmask(stm32::Interrupt::EXTI15_10);
+        };
+        //i2s2 interrupt
+        unsafe {
+            let spi2 = &(*stm32::SPI2::ptr());
+            spi2.cr2.modify(|_, w| {
+                w.txeie().clear_bit();
+                w.rxneie().set_bit();
+                w.errie().set_bit()
+            });
+        }
+        //i2s2_ext interrupt
+        unsafe {
+            let i2s2ext = &(*stm32::I2S2EXT::ptr());
+            i2s2ext.cr2.modify(|_, w| {
+                w.txeie().set_bit();
+                w.rxneie().clear_bit();
+                w.errie().set_bit()
+            });
+        }
+        //i2s2 interrupt in NVIC
+        unsafe {
+            stm32::NVIC::unmask(stm32::Interrupt::SPI2);
+        }
+        //setup spi2 peripheral into i2s mode
+        unsafe {
+            let spi2 = &(*stm32::SPI2::ptr());
+            spi2.i2spr.modify(|_, w| {
+                w.i2sdiv().bits(I2SDIV);
+                w.odd().bit(ODD);
+                w.mckoe().bit(MCK)
+            });
+            spi2.i2scfgr.modify(|_, w| {
+                w.i2smod().i2smode(); //
+                w.i2scfg().master_rx(); //
+                w.pcmsync().long(); //
+                w.i2sstd().philips(); //
+                w.ckpol().idle_high(); //
+                w.datlen().twenty_four_bit(); //
+                w.chlen().thirty_two_bit(); //
+                w.i2se().enabled()
+            })
+        }
+        //setup i2s2ext peripheral
+        unsafe {
+            let i2s2ext = &(*stm32::I2S2EXT::ptr());
+            i2s2ext.i2spr.modify(|_, w| {
+                w.i2sdiv().bits(I2SDIV);
+                w.odd().bit(ODD);
+                w.mckoe().bit(MCK)
+            });
+            i2s2ext.i2scfgr.modify(|_, w| {
+                w.i2smod().i2smode(); //
+                w.i2scfg().slave_tx(); //
+                w.pcmsync().long(); //
+                w.i2sstd().philips(); //
+                w.ckpol().idle_high(); //
+                w.datlen().twenty_four_bit(); //
+                w.chlen().thirty_two_bit(); //
+                w.i2se().enabled()
+            })
+        }
     }
 
     #[idle]
