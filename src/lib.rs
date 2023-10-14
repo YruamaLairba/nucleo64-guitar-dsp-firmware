fc6df2b5e394737d66a92477017d24aa40a11487
Yruama_Lairba <yruama_lairba@hotmail.com>
2023-10-14 22:38:53 +0200

    method to read and write audio sample directly to shared dma

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 pub mod dma;
+use dma::DmaCell;
 
 /// 2 channel AudioSample
 #[derive(Copy, Clone, Debug)]
@@ -23,6 +24,7 @@ impl AudioSample {
 }
 
 impl AudioSample {
+    /// Create an AudioSample from an array that represent a full i2s frame.
     pub fn from_i2s(i2s: [u16; 4]) -> Self {
         let l_msb = i2s[0];
         let l_lsb = i2s[1];
@@ -35,7 +37,23 @@ impl AudioSample {
         Self([l, r])
     }
 
-    pub fn to_i2s(self) -> [u16; 4] {
+    /// Create an AudioSample from an array of dma cells that contain data of a full i2s frame.
+    ///
+    /// The caller shoudld ensure that the array is not written elsewhere to get a value that
+    /// actually correspond to i2s data.
+    pub fn read_from_i2s_dma_cells(i2s: &[DmaCell<u16>; 4]) -> Self {
+        let l_msb = i2s[0].read();
+        let l_lsb = i2s[1].read();
+        let r_msb = i2s[2].read();
+        let r_lsb = i2s[3].read();
+        let l = (((l_msb as u32) << 16) + (l_lsb as u32)) as i32;
+        let r = (((r_msb as u32) << 16) + (r_lsb as u32)) as i32;
+        let l = (l as f32) / ((i32::MAX as f32) + 1.0);
+        let r = (r as f32) / ((i32::MAX as f32) + 1.0);
+        Self([l, r])
+    }
+
+    pub fn into_i2s(self) -> [u16; 4] {
         let l = self.0[0];
         let r = self.0[1];
         // Note, big float are saturated by the language to fit in int
@@ -46,6 +64,26 @@ impl AudioSample {
         let r_msb = (r >> 16) as u16;
         let r_lsb = (r & 0x0000_FFFF) as u16;
         [l_msb, l_lsb, r_msb, r_lsb]
+    }
+
+    /// Convert an AudioSample to raw i2s data and write it to the passed array of dma cells.
+    ///
+    /// The caller shoudld ensure that the array is not written elsewhere to produce i2s data that
+    /// actually correspond to the AudioSample.
+    pub fn write_to_i2s_dma_cells(&self, i2s: &[DmaCell<u16>; 4]) {
+        let l = self.0[0];
+        let r = self.0[1];
+        // Note, big float are saturated by the language to fit in int
+        let l = (l * ((i32::MAX as f32) + 1.0)) as i32;
+        let r = (r * ((i32::MAX as f32) + 1.0)) as i32;
+        let l_msb = (l >> 16) as u16;
+        let l_lsb = (l & 0x0000_FFFF) as u16;
+        let r_msb = (r >> 16) as u16;
+        let r_lsb = (r & 0x0000_FFFF) as u16;
+        i2s[0].write(l_msb);
+        i2s[1].write(l_lsb);
+        i2s[2].write(r_msb);
+        i2s[3].write(r_lsb);
     }
 }
 
